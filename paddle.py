f82d63837ece502b5b54efc181ef62170deaf72a
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-04-11 11:43:12 -0400

    Rename paddle rect from paddle to hitbox

--- paddle.py
+++ paddle.py
@@ -5,14 +5,14 @@ class Paddle():
     WIDTH = 10
 
     def __init__(self, x, y):     
-        x_pos, y_pos = max(0, x - Paddle.LENGTH), y - Paddle.LENGTH/2
-        self.paddle = pygame.Rect(x_pos, y_pos, Paddle.WIDTH, Paddle.LENGTH)
+        x_pos, y_pos = max(0, x - Paddle.WIDTH), y - Paddle.LENGTH/2
+        self.hitbox = pygame.Rect(x_pos, y_pos, Paddle.WIDTH, Paddle.LENGTH)
 
     def move_up(self, change, screen):     
-        if self.paddle.top + change >= 0:
-            self.paddle.move_ip(0, -change)
+        if self.hitbox.top + change >= 0:
+            self.hitbox.move_ip(0, -change)
 
     def move_down(self, change, screen):
         _, height = screen.get_size()
-        if self.paddle.bottom + change <= height:
-            self.paddle.move_ip(0, change)
+        if self.hitbox.bottom + change <= height:
+            self.hitbox.move_ip(0, change)
