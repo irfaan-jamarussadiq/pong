f1b6c83c3b592b793afffa0e4c665e1d4ba8f7d6
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-04-11 06:33:06 -0400

    Refactor static constant naming and game instance

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import pygame, sys
 from game import Game
 
-width = 750
-height = 500
-frame_rate = 60
+WIDTH = 750
+HEIGHT = 500
+FRAME_RATE = 60
+PADDLE_DISPLACEMENT = 5
 
 pygame.init()
-
-my_game = Game(width, height, 'Player', 'Opponent')
-
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Pong')
 clock = pygame.time.Clock()
-displacement = 5
+pong_game = Game(WIDTH, HEIGHT)
 
 while True:   
     for event in pygame.event.get():             
@@ -21,14 +19,14 @@ while True:
             sys.exit()
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        my_game.player.move_up(displacement, screen)
+        pong_game.player.move_up(PADDLE_DISPLACEMENT, screen)
     elif keys[pygame.K_d]:
-        my_game.player.move_down(displacement, screen)  
+        pong_game.player.move_down(PADDLE_DISPLACEMENT, screen)  
     elif keys[pygame.K_p]:
-        my_game.opponent.move_up(displacement, screen)
+        pong_game.opponent.move_up(PADDLE_DISPLACEMENT, screen)
     elif keys[pygame.K_l]:
-        my_game.opponent.move_down(displacement, screen) 
+        pong_game.opponent.move_down(PADDLE_DISPLACEMENT, screen) 
     
-    my_game.draw(screen)
+    pong_game.draw(screen)
     pygame.display.update()
-    clock.tick(frame_rate)
+    clock.tick(FRAME_RATE)
