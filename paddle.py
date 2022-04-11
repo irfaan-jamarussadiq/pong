f33026d52386d3b46f882f04cabee4bc211ea152
Irfaan Jamarussadiq <irfaanj@umd.edu>
2022-04-11 10:04:14 -0400

    Rename player class to paddle and move constants

---
+++ paddle.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Paddle():
+    LENGTH = 80
+    WIDTH = 10
+
+    def __init__(self, x, y, name):     
+        x_pos, y_pos = max(0, x - Paddle.LENGTH), y - Paddle.LENGTH/2
+        self.paddle = pygame.Rect(x_pos, y_pos, Paddle.WIDTH, Paddle.LENGTH)
+        self.name = name
+
+    def move_up(self, change, screen):     
+        if self.paddle.top + change >= 0:
+            self.paddle.move_ip(0, -change)
+
+    def move_down(self, change, screen):
+        _, height = screen.get_size()
+        if self.paddle.bottom + change <= height:
+            self.paddle.move_ip(0, change)
+
+    def __eq__(self, other):
+        return isinstance(other, Paddle) and self.name == other.name
